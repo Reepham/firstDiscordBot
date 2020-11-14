8b046e3e6919abe3b8a90f3e13b90fe8b858f445
Reepham <timo.hannemann@hani-web.de>
2020-11-14 14:55:51 +0100

    Fix vom Join Event - Bessere Join Nachricht

--- src/main/java/events/JoinEvent.java
+++ src/main/java/events/JoinEvent.java
@@ -2,8 +2,8 @@ package events;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.TextChannel;
-import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -15,13 +15,13 @@ public class JoinEvent extends ListenerAdapter {
     @Override
     public void onGuildMemberJoin(GuildMemberJoinEvent event) {
         Guild guild = event.getGuild(); // Get the guild that the user joined.
-        User user = event.getUser();    // Get the user that joined.
+        Member member = event.getMember();    // Get the user that joined.
         JDA client = event.getJDA();    // Get the already existing JDA instance.
 
-        List<TextChannel> channels = guild.getTextChannelsByName("Allgemein", true); // Get the list of channels in the guild that matches that name.
+        List<TextChannel> channels = guild.getTextChannelsByName("general", true); // Get the list of channels in the guild that matches that name.
 
         for (TextChannel channel : channels) { // Loops through the channels and sends a message to each one.
-            channel.sendMessage("New member joined: " + user).queue();
+            channel.sendMessage("Herzlich Willkommen " + event.getMember().getAsMention() + "!\nStell dich doch mal vor ^^").queue();
         }
     }
 }
